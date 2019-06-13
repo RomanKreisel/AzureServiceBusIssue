02f0230938fb9bdfa89c55e9e773bdc85f5dff0b
Roman Kreisel <roman.kreisel@festo.com>
2019-06-13 09:22:28 +0200

    Better default settings

--- MessageReceiver/MessageReceiver/Options/ServiceBusOptions.cs
+++ MessageReceiver/MessageReceiver/Options/ServiceBusOptions.cs
@@ -2,14 +2,14 @@
 {
     public class ServiceBusOptions
     {
-        public int SimulateProcessingMilliseconds = 0;
+        public int SimulateProcessingMilliseconds = 300;
         public string ConnectionString { get; set; }
 
         public string QueueName { get; set; }
 
-        public int PrefetchCount { get; set; } = 100;
+        public int PrefetchCount { get; set; } = 2;
 
-        public int Workers { get; set; } = 2;
+        public int Workers { get; set; } = 10;
 
 
         public int MaxAutoRenewSeconds { get; set; } = 60;
